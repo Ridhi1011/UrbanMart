937e54db9f574237ba1424274c20d2ceb4fbd583
Ridhi1011 <iridhigarg.com>
2026-04-18 23:37:34 +0530

    fix cors properly

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,11 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'https://urban-mart-xy5d.vercel.app',
+  credentials: true,
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
